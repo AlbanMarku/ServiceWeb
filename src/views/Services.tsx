67fd41fca607bd1b784b6a8c52d0f1f5b31dd26b
AlbanMarku <albanmarku@outlook.com>
2023-11-04 14:59:28 +0000

    service banner

--- src/views/Services.tsx
+++ src/views/Services.tsx
@@ -1,7 +1,12 @@
+import serviceBanner from '../assets/serviceBanner.jpg';
+import '../styles/services.css';
+
 export default function Services() {
   return (
     <div className="Services">
-      <h1>hello world</h1>
+      <div className="BannerContainer">
+        <img id="Banner" src={serviceBanner} alt="services" />
+      </div>
     </div>
   );
 }

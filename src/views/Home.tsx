a4868e810fb97fe2384ae20ea455ba79e27a51b8
AlbanMarku <albanmarku@outlook.com>
2023-12-29 14:23:10 +0000

    chat icon

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Fab } from '@mui/material';
+import { FaWhatsapp } from 'react-icons/fa6';
 import WelcomeBanner from '../components/WelcomeBanner';
 import ServiceBox from '../components/ServiceBox';
 import OurWork from '../components/OurWork';
@@ -5,15 +8,23 @@ import Socials from '../components/Socials';
 import AboutUs from '../components/AboutUs';
 import CanvasComp from '../components/CanvasComp';
 
+import '../styles/home.css';
+
 export default function Home() {
   return (
-    <div className="Home">
+    <div className="Home" style={{ position: 'relative' }}>
       <CanvasComp />
       <WelcomeBanner />
       <Socials />
       <AboutUs />
       <ServiceBox />
       <OurWork />
+
+      <Fab className="chat-icon" color="primary" aria-label="add">
+        <a className="chat-inner" href="https://wa.me/message/JVQ7EOQN4QRQH1" target="_blank" rel="noreferrer">
+          <FaWhatsapp color="white" size={25} />
+        </a>
+      </Fab>
     </div>
   );
 }
